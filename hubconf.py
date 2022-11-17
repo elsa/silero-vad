2dd5cbe1e24e7672bc0e967bbcd8677b23127307
Jorge Proença <34271399+jdlproenca@users.noreply.github.com>
2022-11-17 15:43:40 +0000

    Revert "Merge pull request #255 from snakers4/adamnsandle"
    This reverts commit 51b5245410a4b305f7db0b182741bb438599b1f1, reversing
    changes made to c7eab751b5f7e463cd17ec9e6a6c328c6bf3e4ee.

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,6 @@
 dependencies = ['torch', 'torchaudio']
 import torch
 import json
-import warnings
 from utils_vad import (init_jit_model,
                        get_speech_timestamps,
                        get_number_ts,
@@ -17,7 +16,7 @@ from utils_vad import (init_jit_model,
 
 
 def versiontuple(v):
-    return tuple(map(int, (v.split('+')[0].split("."))))
+    return tuple(map(int, (v.split("."))))
 
 
 def silero_vad(onnx=False, force_onnx_cpu=False):
@@ -26,11 +25,11 @@ def silero_vad(onnx=False, force_onnx_cpu=False):
     Please see https://github.com/snakers4/silero-vad for usage examples
     """
 
-    if not onnx:
-        installed_version = torch.__version__
-        supported_version = '1.12.0'
-        if versiontuple(installed_version) < versiontuple(supported_version):
-            warnings.warn(f'Please install torch {supported_version} or greater ({installed_version} installed)')
+    installed_version = torch.__version__
+    supported_version = '1.12.0'
+
+    if versiontuple(installed_version) < versiontuple(supported_version):
+        raise Exception(f'Please install torch {supported_version} or greater ({installed_version} installed)')
 
     hub_dir = torch.hub.get_dir()
     if onnx:
