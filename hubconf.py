afa243d7e552df946d203534f0f46c42006900d8
Jorge Proença <34271399+jdlproenca@users.noreply.github.com>
2023-05-15 14:46:00 +0100

    Update hubconf.py
    Quick fix

--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,7 @@
 dependencies = ['torch', 'torchaudio']
 import torch
 import json
+import os
 from utils_vad import (init_jit_model,
                        get_speech_timestamps,
                        get_number_ts,
